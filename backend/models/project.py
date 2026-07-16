0427d23962b52be44ba2ccb33faf0d8d020ecf9a
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2026-07-16 10:33:47 +0100

    Add ProjectCreate and Project models using Pydantic

--- backend/models/project.py
+++ backend/models/project.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel
 
+
+class ProjectCreate(BaseModel):
+    title: str
+    field: str
+    description: str
+    target_journal: str
+
+
+class Project(ProjectCreate):
+    id: str
