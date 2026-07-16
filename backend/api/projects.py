5908f3968645fa8f80377e60c7f0cdaf08659fd2
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2026-07-16 15:16:05 +0100

    Refactor project API to use database session

--- backend/api/projects.py
+++ backend/api/projects.py
@@ -1,27 +1,24 @@
-from fastapi import APIRouter
-from uuid import uuid4
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from backend.models.project import ProjectCreate, Project
+from backend.crud.projects import create_project, get_projects
+from backend.database.session import get_db
+from backend.models.project import ProjectCreate
 
 router = APIRouter(
     prefix="/projects",
     tags=["Projects"]
 )
 
-projects: list[Project] = []
+
+@router.get("/")
+def list_projects(db: Session = Depends(get_db)):
+    return get_projects(db)
 
 
-@router.get("/", response_model=list[Project])
-def list_projects():
-    return projects
-
-
-@router.post("/", response_model=Project)
-def create_project(project: ProjectCreate):
-    new_project = Project(
-        id=str(uuid4()),
-        **project.model_dump()
-    )
-
-    projects.append(new_project)
-    return new_project
+@router.post("/")
+def add_project(
+    project: ProjectCreate,
+    db: Session = Depends(get_db)
+):
+    return create_project(db, project)
