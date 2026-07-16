436823e2bf425fb4417322ced2c8433bf0d483b9
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2026-07-16 14:42:41 +0100

    Load environment variables in config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,1 +1,16 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+
+class Settings:
+    APP_NAME = "ImoleWrites Research Platform"
+    APP_VERSION = "2.0.0"
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+settings = Settings()
