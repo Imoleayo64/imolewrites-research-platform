bcafb083a4e10f05b8118609c80ab6bb350f413c
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2026-07-16 15:14:02 +0100

    Implement CRUD functions for project management

--- backend/crud/projects.py
+++ backend/crud/projects.py
@@ -1,1 +1,23 @@
+from sqlalchemy.orm import Session
 
+from backend.database.models import ProjectModel
+from backend.models.project import ProjectCreate
+
+
+def create_project(db: Session, project: ProjectCreate):
+    db_project = ProjectModel(
+        title=project.title,
+        field=project.field,
+        description=project.description,
+        target_journal=project.target_journal
+    )
+
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+
+    return db_project
+
+
+def get_projects(db: Session):
+    return db.query(ProjectModel).all()
