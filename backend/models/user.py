c296d2d77e7d6ffd0ac67d2fe4a2b0a2b91cb4d0
IBRAHIM AJALA <79182423+Imoleayo64@users.noreply.github.com>
2026-07-16 15:08:39 +0100

    Create User models for user management
    Added UserCreate and User models using Pydantic.

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel, EmailStr
 
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+
+
+class User(BaseModel):
+    id: str
+    full_name: str
+    email: EmailStr
